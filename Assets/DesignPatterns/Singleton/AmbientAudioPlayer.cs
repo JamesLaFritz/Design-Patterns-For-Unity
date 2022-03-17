02903f342906ae1bf1442bfde03d15c7884a541a
James LaFritz <ktmarine1999@gmail.com>
2022-03-17 11:34:41 -0400

    Set Up

---
+++ Assets/DesignPatterns/Singleton/AmbientAudioPlayer.cs
@@ -0,0 +1,13 @@
+// AmbientAudioPlayer.cs
+// 03-17-2022
+// James LaFritz
+
+using UnityEngine;
+
+namespace Singleton
+{
+    public class AmbientAudioPlayer : MonoBehaviour
+    {
+        
+    }
+}
