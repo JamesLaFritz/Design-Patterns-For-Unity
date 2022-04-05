de3d2e4ac26ccdb3baf35cc9803ef0d7c95aed7f
James LaFritz <ktmarine1999@gmail.com>
2022-04-04 20:54:03 -0400

    Play Random Music On Awake.

--- Assets/DesignPatterns/Singleton/PlayRandomMusicOnAwake.cs
+++ Assets/DesignPatterns/Singleton/PlayRandomMusicOnAwake.cs
@@ -2,8 +2,10 @@
 // 04-04-2022
 // James LaFritz
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Singleton
 {
@@ -15,6 +17,16 @@ namespace Singleton
         {
             if (m_audioClips == null || m_audioClips.Count < 1)
             {
+                Destroy(this);
+            }
+
+            try
+            {
+                AmbientAudioPlayer.GetInstance()!.Play(m_audioClips[Random.Range(0, m_audioClips.Count)]);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e);
                 Destroy(this);
             }
         }
