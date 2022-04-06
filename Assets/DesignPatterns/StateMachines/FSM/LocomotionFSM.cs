10fb53ef78fa2922b240d446549a9d3eaf220206
James LaFritz <ktmarine1999@gmail.com>
2022-04-05 20:30:04 -0400

    Set up For locomotion FSM

---
+++ Assets/DesignPatterns/StateMachines/FSM/LocomotionFSM.cs
@@ -0,0 +1,13 @@
+// LocomotionFSM.cs
+// 04-05-2022
+// James LaFritz
+
+using UnityEngine;
+
+namespace DesignPatterns.StateMachines.FSM
+{
+    public class LocomotionFSM : MonoBehaviour
+    {
+        
+    }
+}
