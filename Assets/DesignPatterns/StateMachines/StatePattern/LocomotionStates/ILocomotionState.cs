94a557124c3b6d1c6915b93f0fd7ac4ae900acf4
James LaFritz <ktmarine1999@gmail.com>
2022-04-13 18:35:59 -0400

    Locomotion State Pattern Added.

--- Assets/DesignPatterns/StateMachines/StatePattern/LocomotionStates/ILocomotionState.cs
+++ Assets/DesignPatterns/StateMachines/StatePattern/LocomotionStates/ILocomotionState.cs
@@ -6,8 +6,19 @@ namespace DesignPatterns.StateMachines.StatePattern.LocomotionStates
 {
     public interface ILocomotionState
     {
+        /// <summary>
+        /// This is where you enter the state and do things you only need to do once when you first enter the state.
+        /// </summary>
         public void EnterState(ILocomotionContext context);
+
+        /// <summary>
+        /// This is where you do any clean-up once before the state changes to a new state.
+        /// </summary>
         public void ExitState(ILocomotionContext context);
+
+        /// <summary>
+        /// This is where the core update logic, which executes in every frame the state exists.
+        /// </summary>
         public void UpdateState(ILocomotionContext context);
     }
 }
