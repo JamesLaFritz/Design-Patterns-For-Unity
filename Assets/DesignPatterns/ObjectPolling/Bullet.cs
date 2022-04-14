47af0b691417c453d73bf3966f8dd66479a27397
James LaFritz <ktmarine1999@gmail.com>
2022-04-14 10:26:48 -0400

    Created Bullet Prefab that moves in the X Direction on the screen Once it becomes invisible (goes out of view of the camera) it destroys itself

---
+++ Assets/DesignPatterns/ObjectPolling/Bullet.cs
@@ -0,0 +1,23 @@
+// Bullet.cs
+// 04-14-2022
+// James LaFritz
+
+using UnityEngine;
+
+namespace DesignPatterns.ObjectPolling
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private Vector3 m_speed;
+
+        private void Update()
+        {
+            transform.position += m_speed * Time.deltaTime;
+        }
+
+        private void OnBecameInvisible()
+        {
+            Destroy(this);
+        }
+    }
+}
