17c3332777d82a7adc78f2926f015607d1570a67
James LaFritz <ktmarine1999@gmail.com>
2022-04-24 22:41:02 -0400

    Delayed Decorator

---
+++ Assets/DesignPatterns/DecoratorPattern/DelayedDecorator.cs
@@ -0,0 +1,30 @@
+// DelayedDecorator.cs
+// 04-24-2022
+// James LaFritz
+
+using DesignPatterns.StrategyPattern;
+
+namespace DesignPatterns.DecoratorPattern
+{
+    public class DelayedDecorator : IAbility
+    {
+        private IAbility m_ability;
+
+        public DelayedDecorator(IAbility ability)
+        {
+            m_ability = ability;
+        }
+
+        #region Implementation of IAbility
+
+        /// <inheritdoc />
+        public void Use()
+        {
+            // Do Delaying magic
+
+            m_ability.Use();
+        }
+
+        #endregion
+    }
+}
