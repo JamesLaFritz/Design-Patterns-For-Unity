934378577f62774eb6c8dda8f3af3ea3f20c521f
James LaFritz <ktmarine1999@gmail.com>
2022-04-27 16:15:53 -0400

    created Parralel Ability

---
+++ Assets/DesignPatterns/CompositePattern/ParallelAbility.cs
@@ -0,0 +1,18 @@
+// ParallelAbility.cs
+// 04-27-2022
+// James LaFritz
+
+using DesignPatterns.StrategyPattern;
+
+namespace DesignPatterns.CompositePattern
+{
+    public class ParallelAbility : IAbility
+    {
+        #region Implementation of IAbility
+
+        /// <inheritdoc />
+        public void Use() { }
+
+        #endregion
+    }
+}
