0e7700df85dc531e9daeac523b4d75fbedd815a9
James LaFritz <ktmarine1999@gmail.com>
2022-04-27 16:00:19 -0400

    created Sequence Ability

---
+++ Assets/DesignPatterns/CompositePattern/SequenceAbility.cs
@@ -0,0 +1,18 @@
+// SequenceAbility.cs
+// 04-27-2022
+// James LaFritz
+
+using DesignPatterns.StrategyPattern;
+
+namespace DesignPatterns.CompositePattern
+{
+    public class SequenceAbility : IAbility
+    {
+        #region Implementation of IAbility
+
+        /// <inheritdoc />
+        public void Use() { }
+
+        #endregion
+    }
+}
