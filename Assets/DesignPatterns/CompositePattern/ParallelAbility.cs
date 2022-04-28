5435d2497695b5da4f56d97b10b78663202ab077
James LaFritz <ktmarine1999@gmail.com>
2022-04-28 02:43:35 -0400

    Fixed Parallel Ability to Run tasks correctly. Changed ability => new Task(ability.Use) To ability => Task.Run(ability.Use)

--- Assets/DesignPatterns/CompositePattern/ParallelAbility.cs
+++ Assets/DesignPatterns/CompositePattern/ParallelAbility.cs
@@ -3,10 +3,12 @@
 // James LaFritz
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DesignPatterns.StrategyPattern;
+using UnityEngine;
 
 namespace DesignPatterns.CompositePattern
 {
@@ -37,18 +39,17 @@ namespace DesignPatterns.CompositePattern
         {
             if (m_abilitiesRunning) return;
 
-            if (m_abilities == null || 0 < m_abilities.Length) return;
+            if (m_abilities == null || m_abilities.Length < 1) return;
 
             m_abilitiesRunning = true;
 
-            List<Task> abilityTasks = m_abilities!.Select(ability => new Task(new Action(() => ability.Use()))).ToList();
+            List<Task> abilityTasks = m_abilities.Select(ability => Task.Run(ability.Use)).ToList();
 
             while (abilityTasks.Count > 0)
             {
                 Task finishedTask = await Task.WhenAny(abilityTasks);
                 abilityTasks.Remove(finishedTask);
             }
-
 
             m_abilitiesRunning = false;
         }
