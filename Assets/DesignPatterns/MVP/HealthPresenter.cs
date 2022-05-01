a1897887432512d237a806913facf8d3c40d3501
James LaFritz <ktmarine1999@gmail.com>
2022-05-01 15:42:25 -0400

    Make Sure Health Presenter has a Health to present

--- Assets/DesignPatterns/MVP/HealthPresenter.cs
+++ Assets/DesignPatterns/MVP/HealthPresenter.cs
@@ -10,5 +10,16 @@ namespace DesignPatterns.MVP
     public class HealthPresenter : MonoBehaviour
     {
         [SerializeField] private Health m_health;
+
+        private void Awake()
+        {
+            if (m_health == null)
+            {
+                Debug.LogWarning(
+                    "Health Presenter needs a Health to present please make sure one is set in The Inspector",
+                    gameObject);
+                enabled = false;
+            }
+        }
     }
 }
