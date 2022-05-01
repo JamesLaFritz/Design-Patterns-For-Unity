3e82bdf35843fdd4c4fbdd74a65d35382b8b51bf
James LaFritz <ktmarine1999@gmail.com>
2022-05-01 15:32:40 -0400

    Added health to health presenter

--- Assets/DesignPatterns/MVP/HealthPresenter.cs
+++ Assets/DesignPatterns/MVP/HealthPresenter.cs
@@ -2,13 +2,13 @@
 // 05-01-2022
 // James LaFritz
 
-using System.Diagnostics.CodeAnalysis;
+using Observer;
 using UnityEngine;
 
 namespace DesignPatterns.MVP
 {
     public class HealthPresenter : MonoBehaviour
     {
-        
+        [SerializeField] private Health m_health;
     }
 }
