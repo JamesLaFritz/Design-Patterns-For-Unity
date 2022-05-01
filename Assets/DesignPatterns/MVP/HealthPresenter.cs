29ec6bb4eb179e67ddf799d36b50ec1e03415459
James LaFritz <ktmarine1999@gmail.com>
2022-05-01 15:29:05 -0400

    Created Health Presenter

---
+++ Assets/DesignPatterns/MVP/HealthPresenter.cs
@@ -0,0 +1,14 @@
+// HealthPresenter.cs
+// 05-01-2022
+// James LaFritz
+
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace DesignPatterns.MVP
+{
+    public class HealthPresenter : MonoBehaviour
+    {
+        
+    }
+}
