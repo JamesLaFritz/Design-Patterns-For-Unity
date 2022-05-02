7078867faf9261f33536d63a8d59a53fbefbc584
James LaFritz <ktmarine1999@gmail.com>
2022-05-02 15:33:16 -0400

    Make sure that the LevelPresenter.cs has all needed information set in the inspector.

--- Assets/DesignPatterns/MVP/LevelPresenter.cs
+++ Assets/DesignPatterns/MVP/LevelPresenter.cs
@@ -17,5 +17,32 @@ namespace DesignPatterns.MVP
         [SerializeField] private Text m_levelText;
 
         [SerializeField] private Button m_gainXPButton;
+
+        private void Awake()
+        {
+            if (m_level == null)
+            {
+                Debug.LogWarning(
+                    "Level Presenter needs a Level to present please make sure one is set in The Inspector",
+                    gameObject);
+                enabled = false;
+            }
+
+            if (m_xpSlider == null)
+            {
+                Debug.LogWarning(
+                    "Level Presenter needs a Slider to Update please make sure one is set in The Inspector",
+                    gameObject);
+                enabled = false;
+            }
+
+            if (m_levelText == null)
+            {
+                Debug.LogWarning(
+                    "Level Presenter needs a Text to Update please make sure one is set in The Inspector",
+                    gameObject);
+                enabled = false;
+            }
+        }
     }
 }
