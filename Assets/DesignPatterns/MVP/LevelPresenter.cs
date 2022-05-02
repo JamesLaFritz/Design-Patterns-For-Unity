e3b9556ba2ebc381668a073be12843e2c22dd2ef
James LaFritz <ktmarine1999@gmail.com>
2022-05-02 15:22:23 -0400

    Level Presenter Created

---
+++ Assets/DesignPatterns/MVP/LevelPresenter.cs
@@ -0,0 +1,14 @@
+// LevelPresenter.cs
+// 05-02-2022
+// James LaFritz
+
+using Observer;
+using UnityEngine;
+
+namespace DesignPatterns.MVP
+{
+    public class LevelPresenter : MonoBehaviour
+    {
+        [SerializeField] private Level m_level;
+    }
+}
