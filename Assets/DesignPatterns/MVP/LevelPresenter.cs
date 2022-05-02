09ecbbb64065e2e465f1e0fdcf4800e8adec4122
James LaFritz <ktmarine1999@gmail.com>
2022-05-02 15:28:53 -0400

    View added to the Presenter

--- Assets/DesignPatterns/MVP/LevelPresenter.cs
+++ Assets/DesignPatterns/MVP/LevelPresenter.cs
@@ -4,11 +4,18 @@
 
 using Observer;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DesignPatterns.MVP
 {
     public class LevelPresenter : MonoBehaviour
     {
         [SerializeField] private Level m_level;
+
+        [SerializeField] private Slider m_xpSlider;
+
+        [SerializeField] private Text m_levelText;
+
+        [SerializeField] private Button m_gainXPButton;
     }
 }
